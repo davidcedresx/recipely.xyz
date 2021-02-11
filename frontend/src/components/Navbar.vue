1452e52ef26237a5a1ca07f5bb0d54dd39d92b07
David Cedres <david.cedresx@gmail.com>
2021-02-10 22:57:42 -0400

    configure sass and navbar links

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,10 +1,12 @@
 <script>
 import { reactive } from "vue"
+import { useRoute } from "vue-router"
 
 export default {
   name: "Navbar",
   setup() {
     const state = reactive({ open: false })
+    const route = useRoute()
 
     function logout() {
       localStorage.removeItem("token")
@@ -14,7 +16,12 @@ export default {
       state.open = !state.open
     }
 
-    return { state, logout, toggleOpen }
+    function routeIs(name) {
+      console.log(route.name)
+      return route.name === name
+    }
+
+    return { state, logout, toggleOpen, routeIs }
   }
 }
 </script>
@@ -42,26 +49,39 @@ export default {
 
     <div id="navbar" class="navbar-menu" :class="{ 'is-active': state.open }">
       <div class="navbar-start">
-        <router-link class="navbar-item mr-4" to="/recipes"
-          ><i class="fa fa-book pr-2" /> Recipes</router-link
+        <router-link
+          class="navbar-item mr-4"
+          :class="{ 'is-active': routeIs('recipes') }"
+          to="/recipes"
         >
-        <router-link class="navbar-item" to="/ingredients">
+          <i class="fa fa-book pr-2" />
+          Recipes
+        </router-link>
+        <router-link
+          class="navbar-item"
+          :class="{ 'is-active': routeIs('ingredients') }"
+          to="/ingredients"
+        >
           <i class="fa fa-bread-slice pr-2" />
-          Ingredients</router-link
-        >
+          Ingredients
+        </router-link>
       </div>
 
       <div class="navbar-end">
         <div class="navbar-item">
           <div class="buttons">
             <router-link to="/settings" class="button is-primary">
-              <strong><i class="fa fa-cog mr-1" /> Settings</strong>
+              <strong>
+                <i class="fa fa-cog mr-1" />
+                Settings
+              </strong>
             </router-link>
-            <router-link to="/" class="button is-light" @click="logout"
-              ><strong
-                ><i class="fa fa-sign-out-alt mr-1" /> Logout</strong
-              ></router-link
-            >
+            <router-link to="/" class="button is-light" @click="logout">
+              <strong>
+                <i class="fa fa-sign-out-alt mr-1" />
+                Logout
+              </strong>
+            </router-link>
           </div>
         </div>
       </div>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-    { path: '/', component: () => import('./pages/Home.vue') },
-    { path: '/init', component: () => import('./pages/Init.vue') }, 
-    { path: '/recipes', component: () => import('./pages/Recipes.vue') },
-    { path: '/settings', component: () => import('./pages/Settings.vue') },
-    { path: '/ingredients', component: () => import('./pages/Ingredients.vue') },
+    { path: '/', name: 'home', component: () => import('./pages/Home.vue') },
+    { path: '/init', name: 'init', component: () => import('./pages/Init.vue') }, 
+    { path: '/recipes', name: 'recipes', component: () => import('./pages/Recipes.vue') },
+    { path: '/settings', name: 'settings', component: () => import('./pages/Settings.vue') },
+    { path: '/ingredients', name: 'ingredients', component: () => import('./pages/Ingredients.vue') },
 ]
 
 const router = createRouter({
